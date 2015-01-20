1cc5ebd2167f58c0aa6db7c26d72daf0246cd067
Tina Ellis <tina.ellis@getg5.com>
2015-01-19 21:21:49 -0800

    Remove bootstrap css and merge in new custom css

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,7 +6,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fill => [250, 250]
   end
 
   # Choose what kind of storage to use for this uploader:
@@ -34,10 +34,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
